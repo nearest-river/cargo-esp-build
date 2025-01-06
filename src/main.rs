b3a1fd051bfa33c086e6d3046b9c959a2d75bbca
Nate River <river177169@proton.me>
2025-01-07 03:41:02 +0530

    patched trailing args bug

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,13 @@ static RUSTFLAGS: LazyLock<String>=LazyLock::new(|| match env::var("RUSTFLAGS") 
 
 fn main()-> io::Result<()> {
   let args=env::args().collect::<Vec<_>>();
-  let app=App::parse_from(&args[1..]);
+  let trailing_idx=idx_of(&args,"--");
+  let args=match trailing_idx {
+    Some(trailing)=> &args[1..trailing],
+    _=> &args[1..]
+  };
+
+  let app=App::parse_from(args);
   let mut process=Command::new("rustup");
 
   process.env("RUSTFLAGS",&*RUSTFLAGS);
@@ -43,8 +49,10 @@ fn main()-> io::Result<()> {
   }
 
   process.args(["--target",TARGET]);
-  if let Some(args)=extract_trailing_args(&args) {
-    process.args(args);
+  if let Some(trailing_idx)=trailing_idx {
+    if trailing_idx+1<args.len() {
+      process.args(&args[trailing_idx+1..]);
+    }
   }
 
   if app.dry_run {
@@ -61,14 +69,14 @@ fn main()-> io::Result<()> {
   Ok(())
 }
 
-
-fn extract_trailing_args<S: AsRef<str>>(args: &[S])-> Option<&[S]> {
-  let (idx,_)=args.iter()
+fn idx_of<T,U: PartialEq<T>>(slice: &[T],element: U)-> Option<usize> {
+  let (idx,_)=slice.iter()
   .enumerate()
-  .find(|(_,arg)| arg.as_ref()=="--")?;
+  .find(|(_,entity)| element.eq(entity))?;
 
-  Some(&args[idx+1..])
+  Some(idx)
 }
+
 
 
 

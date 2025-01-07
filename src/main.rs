790e74709e60e457881562b89318fb911699c7d2
Nate River <river177169@proton.me>
2025-01-08 02:42:44 +0530

    optimized sub-optimal cli

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-
-use clap::Parser;
 use std::{
   io,
   env,
@@ -8,16 +6,6 @@ use std::{
 };
 
 
-#[derive(Parser)]
-struct App {
-  #[arg(long)]
-  release: bool,
-  #[arg(long)]
-  flash: bool,
-  #[arg(long)]
-  dry_run: bool,
-}
-
 static TARGET: &str="xtensa-esp8266-none-elf";
 static RUSTFLAGS: LazyLock<String>=LazyLock::new(|| match env::var("RUSTFLAGS") {
   Err(_)=> "-C link-arg=-nostartfiles -C link-arg=-Wl,-Tlink.x".to_owned(),
@@ -25,59 +13,52 @@ static RUSTFLAGS: LazyLock<String>=LazyLock::new(|| match env::var("RUSTFLAGS") 
 });
 
 fn main()-> io::Result<()> {
-  let args=env::args().collect::<Vec<_>>();
-  let trailing_idx=idx_of(&args,"--");
-  let args=match trailing_idx {
-    Some(trailing)=> &args[1..trailing],
-    _=> &args[1..]
-  };
-
-  let app=App::parse_from(args);
+  let args=Args::new();
   let mut process=Command::new("rustup");
 
   process.env("RUSTFLAGS",&*RUSTFLAGS);
   process.args(["run","esp","cargo"]);
 
-  if app.flash {
+  if args.flash {
     process.args(["espflash","flash"]);
   } else {
     process.arg("build");
   }
 
-  if app.release {
-    process.arg("--release");
-  }
-
+  process.args(args.args);
   process.args(["--target",TARGET]);
-  if let Some(trailing_idx)=trailing_idx {
-    if trailing_idx+1<args.len() {
-      process.args(&args[trailing_idx+1..]);
-    }
-  }
-
-  if app.dry_run {
-    println!("{:#?}",process);
-    return Ok(());
-  }
 
   let status=process.status()?;
   if !status.success() {
-    let code=status.code().unwrap_or(1);
+    let code=status.code().unwrap_or(io::ErrorKind::Other as _);
     return Err(io::Error::from_raw_os_error(code));
   }
 
   Ok(())
 }
 
-fn idx_of<T,U: PartialEq<T>>(slice: &[T],element: U)-> Option<usize> {
-  let (idx,_)=slice.iter()
-  .enumerate()
-  .find(|(_,entity)| element.eq(entity))?;
-
-  Some(idx)
+#[derive(Default)]
+struct Args {
+  flash: bool,
+  args: Vec<String>
 }
 
+impl Args {
+  fn new()-> Self {
+    let mut this=Self::default();
+    let mut subcommand_occured=false;
 
+    for arg in env::args().skip(1) {
+      match arg.as_str() {
+        "--flash"=> this.flash=true,
+        arg if arg=="esp-build" && !subcommand_occured=> subcommand_occured=true,
+        _=> this.args.push(arg),
+      }
+    }
+
+    this
+  }
+}
 
 
 
